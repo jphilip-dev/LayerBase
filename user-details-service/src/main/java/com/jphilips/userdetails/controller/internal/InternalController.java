f1947d278f6361ac8e6022527523062140b55a5d
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-26 02:13:03 +0800

    Add InternalController endpoints - Update and Delete endpoints

--- user-details-service/src/main/java/com/jphilips/userdetails/controller/internal/InternalController.java
+++ user-details-service/src/main/java/com/jphilips/userdetails/controller/internal/InternalController.java
@@ -3,8 +3,12 @@ package com.jphilips.userdetails.controller.internal;
 import com.jphilips.shared.dto.UserDetailsRequestDto;
 import com.jphilips.shared.dto.UserDetailsResponseDto;
 import com.jphilips.userdetails.dto.cqrs.CreateUserDetailsCommand;
+import com.jphilips.userdetails.dto.cqrs.DeleteUserDetailsCommand;
 import com.jphilips.userdetails.dto.cqrs.GetUserDetailsByIdQuery;
+import com.jphilips.userdetails.dto.cqrs.UpdateUserDetailsCommand;
 import com.jphilips.userdetails.service.common.command.CommonCreateUserDetailsService;
+import com.jphilips.userdetails.service.common.command.CommonDeleteUserDetailsService;
+import com.jphilips.userdetails.service.common.command.CommonUpdateUserDetailsService;
 import com.jphilips.userdetails.service.common.query.CommonGetUserDetailsByIdService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +23,8 @@ public class InternalController {
 
     private final CommonCreateUserDetailsService commonCreateUserDetailsService;
     private final CommonGetUserDetailsByIdService commonGetUserDetailsByIdService;
+    private final CommonUpdateUserDetailsService commonUpdateUserDetailsService;
+    private final CommonDeleteUserDetailsService commonDeleteUserDetailsService;
 
     @PostMapping
     public ResponseEntity<UserDetailsResponseDto> createUserDetails(
@@ -32,6 +38,36 @@ public class InternalController {
         var response = commonCreateUserDetailsService.execute(command);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDetailsResponseDto> updateUserDetails(
+            @PathVariable Long id,
+            @Valid
+            @RequestBody UserDetailsRequestDto userDetailsRequestDto) {
+
+        var command = UpdateUserDetailsCommand.builder()
+                .userDetailsId(id)
+                .userDetailsRequestDto(userDetailsRequestDto)
+                .build();
+
+        var response = commonUpdateUserDetailsService.execute(command);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserDetailsResponseDto> deleteUserDetails(
+            @PathVariable Long id) {
+
+        var command = DeleteUserDetailsCommand.builder()
+                .userDetailsId(id)
+                .build();
+
+        commonDeleteUserDetailsService.execute(command);
+
+        return ResponseEntity.noContent().build();
 
     }
 
