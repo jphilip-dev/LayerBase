ac3da161ae9a25ccbe3bd96397c893c29ecdbd36
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-26 14:33:47 +0800

    Utilized shared .properties, logback and validation messages

--- auth-service/src/main/java/com/jphilips/auth/config/RequestInterceptorConfig.java
+++ auth-service/src/main/java/com/jphilips/auth/config/RequestInterceptorConfig.java
@@ -1,27 +1,8 @@
 package com.jphilips.auth.config;
 
-import feign.RequestInterceptor;
-import org.slf4j.MDC;
-import org.springframework.context.annotation.Bean;
+import com.jphilips.shared.config.RequestInterceptorBase;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RequestInterceptorConfig {
-
-    // Feign interceptor (common for all requests)
-    @Bean
-    public RequestInterceptor mdcRequestInterceptor() {
-        return requestTemplate -> {
-            String requestId = MDC.get("requestId");
-            if (requestId != null) {
-                requestTemplate.header("X-Request-Id", requestId);
-            }
-
-            String userId = MDC.get("userId");
-            if (userId != null) {
-                requestTemplate.header("X-User-Id", userId);
-            }
-        };
-    }
-
+public class RequestInterceptorConfig extends RequestInterceptorBase {
 }
