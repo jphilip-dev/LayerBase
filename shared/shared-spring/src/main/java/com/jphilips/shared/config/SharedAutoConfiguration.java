061960e3b42e558bf507577bb6c8742d9494c097
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-26 15:10:17 +0800

    Add shared spring module

---
+++ shared/shared-spring/src/main/java/com/jphilips/shared/config/SharedAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.jphilips.shared.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jphilips.shared.beans")
+public class SharedAutoConfiguration {
+}
